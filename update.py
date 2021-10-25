92da0acdd7db02444ab3d79a43fee7343ff0d3b3
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-10-25 17:20:14 +0200

    update

--- update.py
+++ update.py
@@ -17,21 +17,33 @@
 import sys, os
 from urllib.request import urlopen
 
+# to edit
+
+updfile = None
+lang = "fr"
+
+# relative path
+
 global PATH
 PATH = os.path.dirname(sys.argv[0])
+
+# languages
 
 fr = {"mkdir done":"dossier {} créé avec succès",
       "mkdir err":"le dossier {} est déjà existant",
       "wget done": "{} téléchargemé avec succès",
-      "cmd err": "commande inconnue ici -> {}"}
+      "cmd err": "commande inconnue ici -> {}",
+      "address err" : "adresse invalide"}
 
 en = {"mkdir done":"folder {} successfully created",
       "mkdir err":"the folder {} is already existing",
       "wget done": "{} successfully downloaded",
-      "cmd err": "unknown command here -> {}"}
+      "cmd err": "unknown command here -> {}",
+      "address err" : "invalid address"}
 
-updfile = "https://raw.githubusercontent.com/pf4-DEV/online-update/main/test.txt"
-lang = fr
+lang = fr if lang == "fr" else en
+
+# system function
 
 def mkdir(name):
     try:
@@ -42,6 +54,8 @@ def mkdir(name):
 def wget(name,address):
     open(PATH + name, 'wb').write(urlopen(address).read())
     print(lang["wget done"].format(name))
+
+# update launcher
 
 def update(updfile):
     l = urlopen(updfile).read().decode("utf-8").split("\n")
@@ -56,4 +70,9 @@ def update(updfile):
         elif commande != "":
             print(lang["cmd err"].format(commande))
 
-update(updfile)
+if updfile is None:
+    while True:
+        try: update(input("-> "))
+        except: print(lang["address err"])
+else:
+    update(updfile)
