2e027c9bcafc0282bf4ba8138f524af45aad7403
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-10-25 15:37:53 +0200

    Update update.py

--- update.py
+++ update.py
@@ -20,8 +20,15 @@ from urllib.request import urlopen
 global PATH
 PATH = os.path.dirname(sys.argv[0])
 
-fr = {"mkdir done":"dossier {} créé avec succès"}
-en = {"mkdir done":"folder {} successfully created"}
+fr = {"mkdir done":"dossier {} créé avec succès",
+      "mkdir err":"le dossier {} est déjà existant",
+      "wget done": "{} téléchargemé avec succès",
+      "cmd err": "commande inconnue ici -> {}"}
+
+en = {"mkdir done":"folder {} successfully created",
+      "mkdir err":"the folder {} is already existing",
+      "wget done": "{} successfully downloaded",
+      "cmd err": "unknown command here -> {}"}
 
 updfile = "https://raw.githubusercontent.com/pf4-DEV/online-update/main/test.txt"
 lang = fr
@@ -30,11 +37,11 @@ def mkdir(name):
     try:
         os.makedirs(PATH+name)
         print(lang["mkdir done"].format(name))
-    except FileExistsError: print(f"le dossier {name} est déjà existant")
+    except FileExistsError: print(lang["mkdir err"].format(name))
 
 def wget(name,address):
     open(PATH + name, 'wb').write(urlopen(address).read())
-    print(f"{name} téléchargemé avec succès")
+    print(lang["wget done"].format(name))
 
 def update(updfile):
     l = urlopen(updfile).read().decode("utf-8").split("\n")
@@ -42,12 +49,11 @@ def update(updfile):
         comp = str(e).split(" ")
         commande = comp[0].strip()
         arg = "".join(comp[1:len(comp)]).split(",")
-        print(commande,arg)
         if commande == "mkd":
             mkdir(arg[0])
         elif commande == "wgt":
             wget(arg[0],arg[1])
         elif commande != "":
-            print(f"commande inconnue ici -> {commande}")
+            print(lang["cmd err"].format(commande))
 
 update(updfile)
