4f6a455ac4f230d3e71e5b54be5e266bc46191fa
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-10-26 12:07:17 +0200

    pf4

--- cli-updater.py
+++ cli-updater.py
@@ -24,7 +24,7 @@ PATH = os.path.dirname(sys.argv[0])
 
 # list of road
 
-road = [""]
+road = ["https://raw.githubusercontent.com/pf4-DEV/online-update/main/road/main.txt"]
 
 # languages
 
@@ -46,28 +46,35 @@ lang = en
 
 # system function
 
-def mkdir(name):
+def mkdir(chem, name):
     try:
-        os.makedirs(PATH+name)
+        os.makedirs(PATH+chem+name)
         print(lang["mkdir done"].format(name))
     except FileExistsError: print(lang["mkdir err"].format(name))
 
-def wget(name,address):
-    open(PATH + name, 'wb').write(urlopen(address).read())
+def wget(chem, name, address):
+    open(PATH + chem + name, 'wb').write(urlopen(address).read())
     print(lang["wget done"].format(name))
+
+def mkchem(chem):
+    dos = chem.split("/")
+    while "" in dos: dos.remove("")
+    for x in range(len(dos)):
+        mkdir("","".join(f"/{dos[y]}" for y in range(x+1)))
 
 # update launcher
 
-def update(updfile):
+def update(chem, updfile):
+    mkchem(chem)
     l = urlopen(updfile).read().decode("utf-8").split("\n")
     for e in l:
         comp = str(e).split(" ")
         commande = comp[0].strip()
         arg = "".join(comp[1:len(comp)]).split(",")
         if commande == "mkd":
-            mkdir(arg[0])
+            mkdir(chem, arg[0])
         elif commande == "wgt":
-            wget(arg[0],arg[1])
+            wget(chem, arg[0],arg[1])
         elif commande != "":
             print(lang["cmd err"].format(commande))
 
@@ -75,13 +82,21 @@ def update(updfile):
 
 while True:
     ipt = input("~} ").split(" ")
+    for _ in range(10 - len(ipt)): ipt.append("")
     commande = ipt[0]
     if commande == "lang":
         if ipt[1] == "en": lang = en
         elif ipt[1] == "fr": lang = fr
         else: print(lang["arg err"].format(ipt[1]))
     elif commande == "dl":
-        try: update(ipt[1])
+        try: update(ipt[1],ipt[2])
         except: print(lang["address err"])
-    elif commande == "road":
+    elif commande in ["road", "r"]:
         if ipt[1] == "list":
+            print(road)
+        elif ipt[1] == "add":
+            road.append(ipt[2])
+        elif ipt[1] == "del":
+            try: road.remove(ipt[2])
+            except: print(lang["address err"])
+        else: print(lang["arg err"].format(ipt[1]))
